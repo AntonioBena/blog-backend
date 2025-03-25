ea748803c291e290c07761fd5aebe8c8c27af50f
Toni <antoniotumbic@gmail.com>
2025-03-25 19:33:10 +0100

    Add UserEntity inheritance Add UserDetailsServiceImpl logic

--- src/main/java/com/job/interview/blog/model/user/UserEntity.java
+++ src/main/java/com/job/interview/blog/model/user/UserEntity.java
@@ -5,7 +5,15 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.io.Serializable;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -14,8 +22,8 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "user_entity")
-public class UserEntity extends AuditingModel //TODO add security roles and other
-        implements Serializable {
+public class UserEntity extends AuditingModel
+        implements Serializable, UserDetails, Principal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_entity_id")
@@ -41,4 +49,74 @@ public class UserEntity extends AuditingModel //TODO add security roles and othe
 
     @Enumerated(EnumType.STRING)
     private UserRole role;
+
+    @Override
+    public String getName() {
+        return email;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        var authority = new SimpleGrantedAuthority(role.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public String fullName(){
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", enabled=" + enabled +
+                ", accountLocked=" + accountLocked +
+                ", role=" + role +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UserEntity that = (UserEntity) o;
+        return enabled == that.enabled && accountLocked == that.accountLocked && Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(password, that.password) && Objects.equals(email, that.email) && role == that.role;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, password, email, enabled, accountLocked, role);
+    }
 }
--- src/main/java/com/job/interview/blog/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/job/interview/blog/service/impl/UserDetailsServiceImpl.java
@@ -2,12 +2,19 @@ package com.job.interview.blog.service.impl;
 
 import com.job.interview.blog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserDetailsServiceImpl {
+public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
-    //User details service
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findUserEntityByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + userEmail + " not found"));
+    }
 }
